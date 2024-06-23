37ae0657a7d004aed6e14f14a8c326f3255c2653
Aarav Bundela <86971097+Ab20zip@users.noreply.github.com>
2024-06-23 22:14:54 +0530

    Add copyright headers

--- src/main/kotlin/com/altiran/concurrentia/utils/ClientMemoryRetriever.kt
+++ src/main/kotlin/com/altiran/concurrentia/utils/ClientMemoryRetriever.kt
@@ -1,3 +1,21 @@
+/**
+ * Concurrentia - Simple and open-source multithreading utility for Java, Kotlin, etc.
+ * Copyright © 2024 Altiran Systems Pvt. Ltd.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.altiran.concurrentia.utils
 
 import net.jafama.FastMath
